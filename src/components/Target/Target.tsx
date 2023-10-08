801e48ddb45db31f08f87b74cfffe984b571663e
Betrayer <stardestroyer@ukr.net>
2023-10-09 02:59:57 +0300

    moved variables out of components

--- src/components/Target/Target.tsx
+++ src/components/Target/Target.tsx
@@ -1,35 +1,45 @@
-import React, { FC, useState } from 'react';
 import './index.scss';
+
+import React, { FC, useEffect, useState } from 'react';
+
+import { handleRandomCoordinates } from '../../helpers/coordinates';
+import targetSize from '../../constants/_target.module.scss';
 import { useDimensions } from '../../hooks/useDimensions';
 
 const Target: FC = () => {
 	const { dimensions } = useDimensions();
-
-	const [coordinates, setCoordinates] = useState({
-		top: 100,
-		left: 100,
-	});
 
 	const minHeight = 100;
 	const maxHeight = dimensions.height - 100;
 	const minWidth = 0;
 	const maxWidth = dimensions.width - 100;
 
+	useEffect(() => {
+		randomCoordinates();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const [coordinates, setCoordinates] = useState({
+		top: 0,
+		left: 0,
+	});
+
 	const randomCoordinates = () => {
 		setCoordinates({
-			top:
-				Math.floor(Math.random() * (maxHeight - minHeight + 1)) +
-				minHeight,
-			left:
-				Math.floor(Math.random() * (maxWidth - minWidth + 1)) +
-				minWidth,
+			top: handleRandomCoordinates(minHeight, maxHeight),
+			left: handleRandomCoordinates(minWidth, maxWidth),
 		});
 	};
 
 	return (
 		<div
 			className='target'
-			style={{ top: coordinates.top, left: coordinates.left }}
+			style={{
+				top: coordinates.top,
+				left: coordinates.left,
+				height: targetSize.targetSize,
+				width: targetSize.targetSize,
+			}}
 			onClick={randomCoordinates}
 		>
 			<span>
